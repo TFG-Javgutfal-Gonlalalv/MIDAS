1492305ebae7e3dc662d6f10d092cb3ee496e778
gonalle <gonlalalv@alum.us.es>
2022-08-01 18:02:54 +0200

    update deploy to heroku

--- main/admin.py
+++ main/admin.py
@@ -1,21 +1,43 @@
 from django.contrib import admin
 
-from main.models import Class, Attribute, Relation, Run
+from main.models import Class, Attribute, Relation, Run, FrequentAttributes, Prepositions, Technicality
 
 admin.site.register(Run)
 
+
 @admin.register(Class)
 class ClassAdmin(admin.ModelAdmin):
-    list_display = ("name", "score","run_fk")
+    list_display = ("name", "score", "run_fk")
     list_filter = ("run_fk",)
     search_fields = ("name",)
+
+
 @admin.register(Attribute)
 class ClassAdmin(admin.ModelAdmin):
-    list_display = ("name", "score","run_fk","class_fk")
+    list_display = ("name", "score", "run_fk", "class_fk")
     search_fields = ("name",)
     list_filter = ("run_fk",)
 
+
 @admin.register(Relation)
 class ClassAdmin(admin.ModelAdmin):
-    list_display = ("class_fk_1", "verb","class_fk_2","run_fk")
+    list_display = ("class_fk_1", "verb", "class_fk_2", "run_fk")
     list_filter = ("run_fk",)
+
+
+@admin.register(FrequentAttributes)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ("name", "type")
+    list_filter = ("name",)
+
+
+@admin.register(Prepositions)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(Technicality)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("name",)
