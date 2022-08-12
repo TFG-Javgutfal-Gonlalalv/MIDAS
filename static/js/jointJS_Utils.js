c45e6676db871dac8009f0de43c3fe66f52b7f05
Javier <superjaviergf@gmail.com>
2022-08-12 21:32:58 +0200

    Actualización de las interacciones con el diagrama

--- static/js/jointJS_Utils.js
+++ static/js/jointJS_Utils.js
@@ -5,13 +5,13 @@ function random_integer(min, max) {
 function load_box(graph, name, pos_x, pos_y){
     var rect = new joint.shapes.standard.Rectangle();
     rect.position(pos_x, pos_y);
-    rect.resize(150, 40);
+    rect.resize(150, 60);
     rect.attr({
         body: {
             fill: 'white'
         },
         label: {
-            text: name,
+            text: name + "\n--------------------\n",
             fill: 'black'
         }
     });
@@ -71,16 +71,37 @@ function updateClass(graph, cell_id, new_class_name){
         var cell_to_update = graph.getCell(cell_id);
         new_class_name = validate_and_correct_class_name(graph, new_class_name);
         // console.log(cell_to_update);
-        cell_to_update.attr({text: { text: new_class_name }, label: { text: new_class_name }});
+        cell_to_update.attr({text: {
+            text: new_class_name + "\n--------------------\n" },
+            label: { text: new_class_name + "\n--------------------\n"
+        }});
+    }
+}
+
+function updateLink(graph, link_id, mult_1, mult_2){
+    if(!(mult_1 == undefined || mult_1 == null || mult_1 === '') && !(mult_2 == undefined || mult_2 == null || mult_2 === '')){
+        var link_to_update = "";
     }
 }
 
 function validate_and_correct_class_name(graph, class_name){
     var graph_json = graph.toJSON();
+
+    var class_in_graph = [];
     for(var i = 0; i < graph_json["cells"].length; i++){
-        if((graph_json["cells"][i]["type"] === "standard.Rectangle") && (graph_json["cells"][i]["attrs"]["label"]["text"] === class_name)){
+        if(graph_json["cells"][i]["type"] === "standard.Rectangle"){
+            class_in_graph.push(graph_json["cells"][i]["attrs"]["label"]["text"].split('\n--------------------\n')[0]);
+        }
+    }
+
+    var contains_name = true;
+    while(contains_name){
+        if(class_in_graph.indexOf(class_name) == -1){
+            contains_name = false;
+        } else {
             class_name += "_copy";
         }
     }
+
     return class_name;
 }
